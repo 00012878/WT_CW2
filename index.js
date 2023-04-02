a2cda640f6f12997a3b25dcbd3d6e42737dd332c
00012878 <115094738+00012878@users.noreply.github.com>
2023-04-02 19:40:50 +0500

    complete working application
    1. Fully functioning CRUD operations
    2. User-friendly front-end interface

--- index.js
+++ index.js
@@ -1,132 +1,16 @@
 const express = require("express")
-const fs = require("fs")
-const { render } = require("pug")
+const students = require("./routes/students.js")
 
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-function id() {
-	return "_" + Math.random().toString(36).substring(2, 9)
-}
 
 app.use("/static", express.static("public/"))
 app.use(express.urlencoded({extended: false}))
+app.use(students)
 
 app.set("view engine", "pug")
-
-app.get("/", (req, res) => {
-	res.render("index.pug")
-})
-
-
-app.get("/students", (req, res) => {
-	fs.readFile("./data/data.json", (err, data) => {
-		if (err) throw err
-		const dt = JSON.parse(data)
-		res.render("students.pug", {students: dt})
-	})
-})
-
-app.get("/add", (req, res) => {
-	res.render("add.pug")
-})
-
-
-app.post("/add", (req, res) => {
-	const fname = req.body.fname
-	const lname = req.body.lname
-	const sex = req.body.sex
-	const major = req.body.major
-	const level = req.body.level
-	
-	if (fname.trim() === "" || lname.trim() === "") {
-		res.render("add.pug", {error: true})	
-	} else {
-		fs.readFile("./data/data.json", (err, data) => {
-			if (err) throw err
-			const dt = JSON.parse(data)
-			dt.push({
-				id: id(),
-				fname, lname, sex,
-				major, level
-			})
-	
-			fs.writeFile("./data/data.json", JSON.stringify(dt), err => {
-				if (err) throw err
-				console.log("success")
-				res.render("add.pug", {success: true})
-			})
-		})
-
-	}
-})
-
-app.get("/students/:id", (req, res) => {
-	const id = req.params.id
-
-	fs.readFile("./data/data.json", (err, data) => {
-		if (err) throw err
-		const dt = JSON.parse(data)
-		const student = dt.filter(item => item.id === id)[0]
-		res.render("detail.pug", {student: student})
-	})
-})
-
-
-app.get("/:id/delete", (req, res) => {
-	const id = req.params.id
-	fs.readFile("./data/data.json", (err, data) => {
-		if (err) throw err
-		const dt = JSON.parse(data)
-		const filtered = dt.filter(item => item.id != id)
-		fs.writeFile("./data/data.json", JSON.stringify(filtered), err => {
-			if (err) throw err
-			res.render("students.pug", {students: filtered, deleted: true})
-		})
-	})
-})
-
-app.post("/:id/update", (req, res) => {
-	const id = req.params.id
-	const fname = req.body.fname
-	const lname = req.body.lname
-	const sex = req.body.sex
-	const major = req.body.major
-	const level = req.body.level
-
-	if (fname.trim() === "" || lname.trim() === "") {
-		fs.readFile("./data/data.json", (err, data) => {
-			if (err) throw err
-			const dt = JSON.parse(data)
-			const student = dt.filter(item => item.id === id)[0]
-			res.render("detail.pug", {student: student, error: true})
-		})
-	} else {
-	fs.readFile("./data/data.json", (err, data) => {
-		if (err) throw err
-		const dt = JSON.parse(data)
-		const student = dt.filter(item => item.id === id)[0]
-
-		const studentId = dt.indexOf(student)
-		const splicedStudent = dt.splice(studentId, 1)[0]
-		splicedStudent.fname = fname
-		splicedStudent.lname = lname
-		splicedStudent.sex = sex
-		splicedStudent.major = major
-		splicedStudent.level = level
-
-		dt.push(splicedStudent)
-
-		fs.writeFile("./data/data.json", JSON.stringify(dt), err => {
-			if (err) throw err
-
-			res.render("students.pug", {students: dt, updated: true})
-		})
-	})
-}
-})
-
 
 app.listen(PORT, () => {
 	console.log(`Serving http://localhost:${PORT}`)
---
+++ routes/students.js
@@ -0,0 +1,126 @@
+const fs = require("fs")
+const express = require("express")
+const router = express.Router()
+
+function id() {
+	return "_" + Math.random().toString(36).substring(2, 9)
+}
+
+router.get("/", (req, res) => {
+	res.render("index.pug")
+})
+
+router.get("/add", (req, res) => {
+	res.render("add.pug")
+})
+
+
+
+router.get("/students", (req, res) => {
+	fs.readFile("./data/data.json", (err, data) => {
+		if (err) throw err
+		const dt = JSON.parse(data)
+		res.render("students.pug", {students: dt})
+	})
+})
+
+
+router.post("/add", (req, res) => {
+	const fname = req.body.fname
+	const lname = req.body.lname
+	const sex = req.body.sex
+	const major = req.body.major
+	const level = req.body.level
+	
+	if (fname.trim() === "" || lname.trim() === "") {
+		res.render("add.pug", {error: true})	
+	} else {
+		fs.readFile("./data/data.json", (err, data) => {
+			if (err) throw err
+			const dt = JSON.parse(data)
+			dt.push({
+				id: id(),
+				fname, lname, sex,
+				major, level
+			})
+	
+			fs.writeFile("./data/data.json", JSON.stringify(dt), err => {
+				if (err) throw err
+				res.render("add.pug", {success: true})
+			})
+		})
+
+	}
+})
+
+router.get("/students/:id", (req, res) => {
+	const id = req.params.id
+
+	fs.readFile("./data/data.json", (err, data) => {
+		if (err) throw err
+		const dt = JSON.parse(data)
+		const student = dt.filter(item => item.id === id)[0]
+		res.render("detail.pug", {student: student})
+	})
+})
+
+router.get("/:id/delete", (req, res) => {
+	const id = req.params.id
+	fs.readFile("./data/data.json", (err, data) => {
+		if (err) throw err
+		const dt = JSON.parse(data)
+		const filtered = dt.filter(item => item.id !== id)
+		fs.writeFile("./data/data.json", JSON.stringify(filtered), err => {
+			if (err) throw err
+			res.render("students.pug", {students: filtered, deleted: true})
+		})
+	})
+})
+
+
+router.post("/:id/update", (req, res) => {
+	const id = req.params.id
+	const fname = req.body.fname
+	const lname = req.body.lname
+	const sex = req.body.sex
+	const major = req.body.major
+	const level = req.body.level
+
+	if (fname.trim() === "" || lname.trim() === "") {
+		fs.readFile("./data/data.json", (err, data) => {
+			if (err) throw err
+			const dt = JSON.parse(data)
+			const student = dt.filter(item => item.id === id)[0]
+			res.render("detail.pug", {student: student, error: true})
+		})
+	} else {
+	fs.readFile("./data/data.json", (err, data) => {
+		if (err) throw err
+		const dt = JSON.parse(data)
+		const student = dt.filter(item => item.id === id)[0]
+
+		const studentId = dt.indexOf(student)
+		const splicedStudent = dt.splice(studentId, 1)[0]
+		splicedStudent.fname = fname
+		splicedStudent.lname = lname
+		splicedStudent.sex = sex
+		splicedStudent.major = major
+		splicedStudent.level = level
+
+		dt.push(splicedStudent)
+
+		fs.writeFile("./data/data.json", JSON.stringify(dt), err => {
+			if (err) throw err
+            fs.readFile("./data/data.json", (err, data) => {
+                if (err) throw err
+                const dt = JSON.parse(data)
+                const student = dt.filter(item => item.id === id)[0]
+                res.render("detail.pug", {student: student, updated: true})
+            })
+			// res.render("detail.pug", {students: dt, updated: true})
+		})
+	})
+}
+})
+
+module.exports = router
